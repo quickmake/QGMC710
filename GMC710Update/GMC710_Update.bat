65813b8a4754ee6364e40d78f0f2ed3ccf7463c9
Wei Shuai <cpuwolf@gmail.com>
2018-12-18 08:51:39 +0800

    add FW update tool

---
+++ GMC710Update/GMC710_Update.bat
@@ -0,0 +1,10 @@
+::<span style="font-family:Arial;font-size:14px;">@ECHO OFF
+@echo off
+title GMC710 Firemware Update Toos       Quick:409050332@qq.com
+echo=
+echo ***************   GMC710 Fileware Update Tools   ***************
+echo=
+echo=
+xdfu.exe -r -c -d --v --fn "QGMC710_181214.dfu"
+
+pause
